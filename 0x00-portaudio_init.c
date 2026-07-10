bc64768fdcbc5fa800e39a4a6212755914ed0098
VPOUR <emmanuellamira20@outlook.com>
2026-07-10 19:48:19 +0300

    oppgave: initisering av PortAudio ute problemer

---
+++ 0x00-portaudio_init.c
@@ -0,0 +1,36 @@
+# include <stdio.h>
+# include <portaudio.h>
+
+/**
+ * main - program that checks if PortAudio has been initialized
+ * successfully using;
+ *  - Pa_Initialize -  to prepare PortAudio to
+ * comunicate with the audio system of the OS by;
+ * 	- initializing Host APIs and setting up available audio backends
+ *   	(JACK, PulseAudio, ALSA etc)
+ * 	- discovers audio devices (all connected sound hardware)
+ * 	- allocate internal memory for real time audio streams
+ * 	- resetting device indices (setting the baseline index
+ *   	used to target specific input and output devices)
+ * Return: 0 if successfully executed
+ */
+
+int main(void)
+{
+	PaError err;
+
+	err = Pa_Initialize();
+	if (err != paNoError)
+	{
+		printf("Feil ved initialisering: %s\n", Pa_GetErrorText(err));
+		return (1);
+	}
+
+	printf("PortAudio startet uten problemer.\n");
+	printf("Versjon: %s\n", Pa_GetVersionText());
+
+	Pa_Terminate();
+	printf("PortAudio avsluttet.\n");
+
+	return (0);
+}
---
+++ 0x01-portaudio_devices.c
@@ -0,0 +1,58 @@
+# include <stdio.h>
+# include <portaudio.h>
+
+/**
+ * main - program that lists all devices that are found by PortAudio
+ * uses functions like;
+ *  - Pa_GetDeviceCount - PortAudio checks for how many audio
+ *  devices it sees, including input and output devices
+ *  - Pa_GetDeviceInfo - for every i, this has a pointer to a struct
+ *  showing full details of the specific device as we loop from 0
+ *  to numDevices
+ *  - Pa_GetDefaultInputDevice, Pa_GetOutputDevice - returns the idex of
+ *  the devices considered standard, used by default under the hood
+ *  Return: 0 if successfully executed
+ */
+
+int main(void)
+{
+	PaError err;
+
+	err = Pa_Initialize();
+	if (err != paNoError)
+	{
+		printf("Feil ved initialisering: %s\n", Pa_GetErrorText(err));
+		return (1);
+	}
+
+	int numDevices = Pa_GetDeviceCount();
+
+	if (numDevices < 0)
+	{
+		printf("Feil: Pa_GetDeviceCount returnerte %d\n", numDevices);
+		Pa_Terminate();
+		return (1);
+	}
+
+	printf("Fant %d lydenheter:\n\n", numDevices);
+
+	for (int i = 0; i < numDevices; i++)
+	{
+		const PaDeviceInfo *info = Pa_GetDeviceInfo(i);
+
+		printf("Enhet %d: %s\n", i, info->name);
+		printf(" Maks input-kanaler: %d\n", info->maxInputChannels);
+		printf(" Maks output-kanaler: %d\n", info->maxOutputChannels);
+		printf(" Standard samplerate: %.0f Hz\n", info->defaultSampleRate);
+		printf("\n");
+	}
+
+	int defaultInput = Pa_GetDefaultInputDevice();
+	int defaultOutput = Pa_GetDefaultOutputDevice();
+
+	printf("Standard input-enhet: %d\n", defaultInput);
+	printf("Standard output-enhet: %d\n", defaultOutput);
+
+	Pa_Terminate();
+	return (0);
+}
